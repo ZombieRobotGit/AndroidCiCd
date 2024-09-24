ad7052ad535d4e49911566bfd1dfcc9d33eaf16b
mgrabysa@listonic.com <mgrabysa@listonic.com>
2024-09-24 07:30:14 +0200

    Add feature 1

--- app/src/main/java/com/example/continuousintegration/MainActivity.kt
+++ app/src/main/java/com/example/continuousintegration/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.continuousintegration
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,6 +26,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("CI/CD Novice")
+                    EditText()
+                    Button()
                 }
             }
         }
@@ -35,6 +40,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun EditText() {
+    OutlinedTextField(value = "enter text", onValueChange = {})
+}
+
+@Composable
+fun Button() {
+    Button(onClick = {
+        Log.d(
+            "TAG",
+            "Button: Button action triggered"
+        )
+    }) {
+
+    }
 }
 
 @Preview(showBackground = true)
